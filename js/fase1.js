a9499f28335f04813dfce187f05b50161d888035
vivianassis <vivianassis11@gmail.com>
2021-07-29 10:25:17 -0300

    first commit

---
+++ js/fase1.js
@@ -0,0 +1,62 @@
+//Operadores aritméticos
+1 + 1
+1 - 1
+1 / 1
+1 * 1
+1 ** 1
+
+//Operadores de igualdade ou relacionais
+1 == 1
+1 !== 1
+1 > 1
+1 < 1
+1 >= 1
+1 <= 1
+
+//Funções
+
+function Nome(parametros) {
+    return Resultado
+}
+
+//Operadores Lógicos
+
+1 || ou //um ou outro é verdade
+&& e //ambos são verdade
+!não //falso
+
+//Operadores unários
+1++ //Soma 1
+1-- //Subtrai 1
+- 1 //valor negativo
+
+//Estrutura Léxica
+
+variavel !== Variavel //Case Sensitive
+/* Comentários de 
+bloco são assim*/
+//Comentários em linha são assim
+
+/*Espaço em branco não é considerado, ficando a cargo de boas práticas
+Ponto e vírgula não é obrigatório, mas opcional*/
+
+
+/*LITERAIS E IDENTIFICADORES E PALAVRAS RESERVADAS
+LITERAL é um valor que é escrito no código-fonte
+IDENTIFICADOR é uma sequência de caracteres que pode ser usada para identificar uma variável, uma função, um objeto. Pode começar com uma letra, um cifrão $ ou um sublinhado _, também podendo conter dígitos.
+PALAVRAS RESERVADAS não podem ser identificadores*/
+
+DICA//O cifrão é comumente usado para fazer referência a elementos DOM (Document Object Model, uma representação de um documento html em nós e objetos).//
+
+//Estruturas condicionais
+
+/*Usa lógica, operadores variados, etc
+Retorna sobrescrição de variável, console.log, alert, etc*/
+if (condição1 == true) {
+    return (tal1)
+} else if/*diversos*/ (condição2 == true) {
+    return (tal2)
+} else {
+    return (exceção)
+}
+
